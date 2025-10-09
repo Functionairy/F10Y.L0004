2df5e3a135c0790411eb7c661b911f62bee41789
davidcoats <david@getsafetycone.com>
2025-10-09 16:16:08 -0700

    Interim commit.

--- source/F10Y.L0004.L000/Code/Values/IFilePaths.cs
+++ source/F10Y.L0004.L000/Code/Values/IFilePaths.cs
@@ -11,21 +11,21 @@ namespace F10Y.L0004.L000
         /// <summary>
         /// <para><value>C:\Temp\Output.csproj</value></para>
         /// </summary>
-        public string Output_ProjectFilePath => @"C:\Temp\Output.csproj";
+        string Output_ProjectFilePath => @"C:\Temp\Output.csproj";
 
         /// <summary>
         /// <para><value>C:\Temp\Output.json</value></para>
         /// </summary>
-        public string Output_JsonFilePath => @"C:\Temp\Output.json";
+        string Output_JsonFilePath => @"C:\Temp\Output.json";
 
         /// <summary>
         /// <para><value>C:\Temp\Output.txt</value></para>
         /// </summary>
-        public string Output_TextFilePath => @"C:\Temp\Output.txt";
+        string Output_TextFilePath => @"C:\Temp\Output.txt";
 
         /// <summary>
         /// <para><value>C:\Temp\Output.xml</value></para>
         /// </summary>
-        public string Output_XmlFilePath => @"C:\Temp\Output.xml";
+        string Output_XmlFilePath => @"C:\Temp\Output.xml";
     }
 }
